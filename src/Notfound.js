3c5bfb8b244ffcc870d9da328eadfe2aacb8c957
DhanushM12 <dhanushmukhi@gmail.com>
2023-11-04 20:03:44 +0530

    Added code of react router

---
+++ src/Notfound.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Notfound() {
+  return (
+    <div>
+        <h2>This is an invalid url</h2>
+        <h3>404 page Not found!</h3>
+        {/* <a href='/'>Home page</a> */}
+    </div>
+  )
+}
+
+export default Notfound
